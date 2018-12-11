2f91abe0f38ef7bbff2fe788ce8fb643e13b7150
Herve Jourdain <herve.jourdain@neuf.fr>
2018-12-11 23:29:10 +0800

    Changed the path to iotivity from "Iotivity" to "iotivity".

--- BloodPressureMonitor/start.sh
+++ BloodPressureMonitor/start.sh
@@ -1,2 +1,2 @@
 cp ./oic_svr_db.dat ./server.dat
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../Iotivity/out/linux/x86_64/release/ ./server
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../iotivity/out/linux/x86_64/release/ ./server
