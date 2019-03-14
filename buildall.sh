509f6e01882930d32a3741e9874a9afd27a9b469
Herve Jourdain <herve.jourdain@neuf.fr>
2019-03-14 22:20:44 +0800

    Changed Iotivity path to "iotivity".

--- buildall.sh
+++ buildall.sh
@@ -1,4 +1,4 @@
-cd Iotivity; scons TARGET_OS=linux TARGET_ARCH=x86_64 TARGET_TRANSPORT=IP RELEASE=1 SECURED=1
+cd iotivity; scons TARGET_OS=linux TARGET_ARCH=x86_64 TARGET_TRANSPORT=IP RELEASE=1 SECURED=1
 cd ../BloodPressureMonitor; ./build.sh
 cd ..
 
